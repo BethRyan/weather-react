23d08a6bc8cfed9b082b06de2ce26dea9aad2f32
Beth Ryan <e.waylett@gmail.com>
2020-07-17 13:46:26 -0500

    re-installing bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -7,13 +7,16 @@ export default function Weather(props) {
   let wind = props.wind;
   let humidity = props.humidity;
   let icon = `https://openweathermap.org/img/wn/${props.icon}@2x.png`;
-  if (temp) {
-    return (
-      <div className="Weather">
-        <h2>The weather in {city}:</h2>
-
-        <img src={icon} alt="weather icon" />
-
+  //if (temp) {
+  return (
+    <div className="Weather">
+      <h2>The weather in {city}:</h2>
+      <div class="row">
+        <div class="col-6">
+          <img src={icon} alt="weather icon" />
+        </div>
+      </div>
+      <div class="col-6">
         <ul id="list">
           <li>Tempurature: {Math.round(temp)}°F</li>
           <li>Description: {description}</li>
@@ -21,8 +24,10 @@ export default function Weather(props) {
           <li>Wind: {Math.round(wind)}mph</li>
         </ul>
       </div>
-    );
-  } else {
-    return <h5>Enter a city</h5>;
-  }
-}
+      <hr class="my-3" />
+    </div>
+  );
+} //else {
+//return <h5>Enter a city</h5>;
+//}
+//}
